7b2f0d1f9f703830b229033379e27bd3e581068c
USER <USER@siyun10>
2020-12-17 18:43:01 +0900

    12.17 study

--- study/src/study/InheritanceApp.java
+++ study/src/study/InheritanceApp.java
@@ -3,10 +3,19 @@ class cal{
 	public int sum(int v1, int v2) {
 		return v1+v2;
 	}
+	public int sum(int v1, int v2, int v3) {
+		return v1+v2+v3;
+	}
 }
 class cal3 extends cal{
 	public int min(int v1, int v2) {
 		return v1-v2;
+	}
+	//overriding
+	public int sum(int v1, int v2) {
+		
+		System.out.println("cal3");
+		return v1+v2;
 	}
 }
 public class InheritanceApp {
@@ -14,10 +23,13 @@ public class InheritanceApp {
 	public static void main(String[] args) {
 		cal c = new cal();
 		System.out.println(c.sum(2, 1));
+		
+		System.out.println(c.sum(2, 1, 1));
 	
 		cal3 c3 = new cal3();
 		System.out.println(c3.sum(2,1));
 		System.out.println(c3.min(2,1));
+		System.out.println(c3.sum(2,1));
 	}
 
 }
