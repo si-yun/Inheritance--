1f652f6391da227c4dba5fb8546548a02d6a6d67
USER <USER@siyun10>
2020-12-18 14:05:05 +0900

    test12

--- study/src/study/InheritanceApp1.java
+++ study/src/study/InheritanceApp1.java
@@ -30,6 +30,10 @@ public class InheritanceApp1 {
 		System.out.println(c4.minus());
 		System.out.println(c4.minus());
 		System.out.println(c4.minus());
+		System.out.println(c4.minus());
+		System.out.println(c4.minus());
+		System.out.println(c4.minus());
+		System.out.println(c4.minus());
 	}
 	//test
 
