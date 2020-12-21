a4cc040a8d90bc7d149f753608fa537b3fafe538
KIMSIYUN <syun.m2@gmail.com>
2020-12-21 09:29:36 +0900

    test1

--- study/src/study/InheritanceApp1.java
+++ study/src/study/InheritanceApp1.java
@@ -7,37 +7,42 @@ class cal1{
 		System.out.println("cal1!");
 	}
 	public int sum() {
+		System.out.println("sum");
 		return this.v1+v2;
 	}
-}
-class cal4 extends cal1{
-
-	cal4(int v1, int v2) {
-		super(v1, v2);   //super�뒗 遺�紐⑦겢�옒�뒪�쓽 �깮�꽦�옄
-		System.out.println("cal4!");
-	}
 	public int minus() {
+		System.out.println("miuns");
 		return this.v1-v2;
 	}
+	public int mul() {
+		System.out.println("mul");
+		return this.v1*v2;
+	}
+	public int div() {
+		System.out.println("div");
+		return this.v1/v2;
+	}
 }
+//class cal4 extends cal1{
+//
+//	cal4(int v1, int v2) {
+//		super(v1, v2);   //super�뒗 遺�紐⑦겢�옒�뒪�쓽 �깮�꽦�옄
+//		System.out.println("cal4!");
+//	}
+//	public int minus() {
+//		return this.v1-v2;
+//	}
+//}
 public class InheritanceApp1 {
 	public static void main(String[] args) {
-		cal1 c1 = new cal1(2,1);
-		cal4 c4 = new cal4(2,1);
-		System.out.println(c4.sum());
-		//test
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
-		//test zz
+		cal1 c1 = new cal1(10,5);
+//		cal4 c4 = new cal4(2,1);
+//		System.out.println(c4.sum());
+		System.out.println(c1.sum());
+		System.out.println(c1.minus());
+		System.out.println(c1.mul());
+		System.out.println(c1.div());
+		
 		
 	}
 
